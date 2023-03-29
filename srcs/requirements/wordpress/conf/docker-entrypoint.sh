8982a5e33f2d0b554dacc186dd58eafef0bc6d4e
Leonardo Moreno <lmoreno@student.42quebec.com>
2023-03-29 17:31:19 -0400

    wp a html

--- srcs/requirements/wordpress/conf/docker-entrypoint.sh
+++ srcs/requirements/wordpress/conf/docker-entrypoint.sh
@@ -11,10 +11,10 @@ while ! mariadb -h$MYSQL_HOSTNAME -u$WP_DB_USER -p$WP_DB_PASSWORD $WP_DB_NAME --
 done
 
 # check if wordpress is installed
-if [ ! -f "/var/www/html/wordpress/wp-config.php" ]; then
+if [ ! -f "/var/www/html/wp-config.php" ]; then
 	echo "[INFO] installing wordpress..."
 
-	wp core download --path=/var/www/html/wordpress --locale=es_CO
+	wp core download --path=/var/www/html --locale=es_CO
 
 	# wp core install --url=$DOMAIN_NAME/wordpress --title="WP Inceptions" \
 	#        	--admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_MAIL
@@ -26,7 +26,7 @@ if [ ! -f "/var/www/html/wordpress/wp-config.php" ]; then
                     --dbhost=$MYSQL_HOSTNAME \
                     --dbcharset="utf8" \
                     --dbcollate="utf8_general_ci" \
-                    --path="/var/www/html/wordpress"
+                    --path="/var/www/html"
     wp core install --allow-root \
                     --title="WP Incepcion" \
                     --admin_name=$WP_ADMIN_USER \
@@ -34,13 +34,13 @@ if [ ! -f "/var/www/html/wordpress/wp-config.php" ]; then
                     --admin_email=$WP_ADMIN_MAIL \
                     --skip-email \
                     --url=$DOMAIN_NAME/wordpress \
-                    --path="/var/www/html/wordpress"
-    # wp user create --allow-root \
-    #                 $WP_DB_USER \
-    #                 $WP_ADMIN_MAIL \
-    #                 --role=author \
-    #                 --user_pass=$WP_DB_PASSWORD \
-    #                 --path="/var/www/html/wordpress"
+                    --path="/var/www/html"
+    wp user create --allow-root \
+                    $WP_USER \
+                    $WP_USER_MAIL \
+                    --role=author \
+                    --user_pass=$WP_USER_PASS \
+                    --path="/var/www/html"
 
     echo "WP Installed"
 
