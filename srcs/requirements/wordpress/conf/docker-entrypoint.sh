ee1e9f0e4182c53f1b34e6d125574f8df7643dc2
Leonardo Moreno <lmoreno@student.42quebec.com>
2023-03-29 19:07:02 -0400

    wp really?

--- srcs/requirements/wordpress/conf/docker-entrypoint.sh
+++ srcs/requirements/wordpress/conf/docker-entrypoint.sh
@@ -11,10 +11,10 @@ while ! mariadb -h$MYSQL_HOSTNAME -u$WP_DB_USER -p$WP_DB_PASSWORD $WP_DB_NAME --
 done
 
 # check if wordpress is installed
-if [ ! -f "/var/www/html/wp-config.php" ]; then
+if [ ! -f "/var/www/html/wp/wp-config.php" ]; then
 	echo "[INFO] installing wordpress..."
 
-	wp core download --path=/var/www/html --locale=es_CO
+	wp core download --path=/var/www/html/wp/ --locale=es_CO
 
 	# wp core install --url=$DOMAIN_NAME/wordpress --title="WP Inceptions" \
 	#        	--admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_MAIL
@@ -26,32 +26,26 @@ if [ ! -f "/var/www/html/wp-config.php" ]; then
                     --dbhost=$MYSQL_HOSTNAME \
                     --dbcharset="utf8" \
                     --dbcollate="utf8_general_ci" \
-                    --path="/var/www/html"
+                    --path="/var/www/html/wp/"
     wp core install --allow-root \
                     --title="WP Incepcion" \
                     --admin_name=$WP_ADMIN_USER \
                     --admin_password=$WP_ADMIN_PASS \
                     --admin_email=$WP_ADMIN_MAIL \
                     --skip-email \
-                    --url=$DOMAIN_NAME \
-                    --path="/var/www/html"
+                    --url=$DOMAIN_NAME/wp/ \
+                    --path="/var/www/html/wp/"
     wp user create --allow-root \
                     $WP_USER \
                     $WP_USER_MAIL \
                     --role=author \
                     --user_pass=$WP_USER_PASS \
-                    --path="/var/www/html"
+                    --path="/var/www/html/wp/"
 
     echo "WP Installed"
-
-
-
 fi
 
 # # enable redis
 # wp redis enable --allow-root
 
-# echo "[INFO] starting php-fpm..."
-# mkdir -p /var/run/php-fpm7
-#php-fpm7 -R --nodaemonize
 php-fpm7 --nodaemonize
