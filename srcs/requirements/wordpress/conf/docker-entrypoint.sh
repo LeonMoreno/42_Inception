96acb632f1d7d5054cf5a28b017c4382d3e56783
Leonardo Moreno <lmoreno@student.42quebec.com>
2023-03-28 21:10:50 -0400

    wp config

--- srcs/requirements/wordpress/conf/docker-entrypoint.sh
+++ srcs/requirements/wordpress/conf/docker-entrypoint.sh
@@ -15,31 +15,37 @@ if [ ! -f "/var/www/html/wp-config.php" ]; then
 	echo "[INFO] installing wordpress..."
 
 	wp core download --path=/var/www/html/wordpress --locale=es_CO
-	cd /var/www/html/wordpress
-	wp config create --dbhost=$MYSQL_HOSTNAME --dbname=$WP_DB_NAME \
-	       	--dbuser=$WP_DB_USER --dbpass=$WP_DB_PASSWORD
-	#wp db install
-	wp core install --url=$DOMAIN_NAME/wordpress --title="WP Inceptions" \
-	       	--admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_MAIL
-# 	wp theme install ryu --activate --allow-root
 
-# 	# redis
-#     sed -i "40i define('WP_REDIS_HOST', '$REDIS_HOST');" wp-config.php
-#     sed -i "41i define('WP_REDIS_PORT', 6379);" wp-config.php
-#     sed -i "42i define('WP_REDIS_PASSWORD', '$REDIS_PWD');" wp-config.php
-#     sed -i "43i define('WP_REDIS_TIMEOUT', 1);" wp-config.php
-#     sed -i "44i define('WP_REDIS_READ_TIMEOUT', 1);" wp-config.php
-#     sed -i "45i define('WP_REDIS_DATABASE', 0);\n" wp-config.php
-# 	sed -i "46i define('WP_CACHE', true);" wp-config.php
-# 	#sed -i "47i define('WP_CACHE_KEY_SALT', 'dpoveda.42.fr');" wp-config.php
-# 	# install redir plugin to wordpress
-#     wp plugin install redis-cache --activate --allow-root
+	# wp core install --url=$DOMAIN_NAME/wordpress --title="WP Inceptions" \
+	#        	--admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_MAIL
 
-# 	# update plugins
-#     wp plugin update --all --allow-root
+	wp config create --allow-root \
+                    --dbname=$WP_DB_NAME \
+                    --dbuser=$WP_DB_USER \
+                    --dbpass=$WP_DB_PASSWORD \
+                    --dbhost=$MYSQL_HOSTNAME \
+                    --dbcharset="utf8" \
+                    --dbcollate="utf8_general_ci" \
+                    --path="/var/www/html"
+    wp core install --allow-root \
+                    --title="WP Incepcion" \
+                    --admin_name=$WP_ADMIN_USER \
+                    --admin_password=$WP_ADMIN_PASS \
+                    --admin_email=$WP_ADMIN_MAIL \
+                    --skip-email \
+                    --url=$DOMAIN_NAME \
+                    --path="/var/www/html"
+    wp user create --allow-root \
+                    $WP_DB_USER \
+                    $WP_ADMIN_MAIL \
+                    --role=author \
+                    --user_pass=$WP_DB_PASSWORD \
+                    --path="/var/www/html"
 
-# 	echo "[INFO] wordpress installation finished"
-# 	touch /var/www/html/$WP_FILE_ONINSTALL
+    echo "WORDPRESS INSTALLED SUCCESSFULLY"
+
+
+
 fi
 
 # # enable redis
